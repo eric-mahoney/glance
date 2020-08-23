afbb7987ea443b2337892ae58f8fc5323dd7c257
ericmahoney <eric.mahoney22@gmail.com>
2020-08-23 05:08:04 -0400

    fixed a bug with minutes < 10 where no zero would show

--- script.js
+++ script.js
@@ -112,6 +112,10 @@ function mapTime(date) {
     date.period = "am";
   }
   date.minute = date.minute += 1; // need to add one to reflect the current minute
+
+  if (date.minute < 10) {
+    date.minute = "0" + date.minute;
+  }
   date.hour = (date.hour + 24) % 12 || 12;
   return date;
 }
